c9d5695304adae57d6ca4815c000ca8288982dcf
datVaulting <meskine.momo@gmail.com>
2021-10-31 18:22:33 +0100

    all is ok

--- docker_push_api.sh
+++ docker_push_api.sh
@@ -1,18 +1,12 @@
 #!/bin/bash -e
 
 
-# this is most likely namespaced repo name like myorg/veryimportantimage
 SOURCE_IMAGE="${DOCKER_REPO_API}"
-# using it as there will be 2 versions published
 TARGET_IMAGE="${DOCKER_REPO_API}"
-# lets make sure we always have access to latest image
 TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
 TIMESTAMP=$(date '+%Y%m%d%H%M%S')
-# using datetime as part of a version for versioned image
 VERSION="${TIMESTAMP}-${TRAVIS_COMMIT}"
-# using specific version as well
-# it is useful if you want to reference this particular version
-# in additional commands like deployment of new Elasticbeanstalk version
+
 TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${VERSION}"
 docker login -u="${dockerid}" -p="${dockerpassword}"
 
--- docker_push_front.sh
+++ docker_push_front.sh
@@ -1,18 +1,11 @@
 #!/bin/bash -e
 
-
-# this is most likely namespaced repo name like myorg/veryimportantimage
 SOURCE_IMAGE="${DOCKER_REPO_FRONT}"
-# using it as there will be 2 versions published
 TARGET_IMAGE="${DOCKER_REPO_FRONT}"
-# lets make sure we always have access to latest image
 TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
 TIMESTAMP=$(date '+%Y%m%d%H%M%S')
-# using datetime as part of a version for versioned image
 VERSION="${TIMESTAMP}-${TRAVIS_COMMIT}"
-# using specific version as well
-# it is useful if you want to reference this particular version
-# in additional commands like deployment of new Elasticbeanstalk version
+
 TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${VERSION}"
 docker login -u="${dockerid}" -p="${dockerpassword}"
 
